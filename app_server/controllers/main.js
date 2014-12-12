b52f0cf636d2eef3c4b5719cc9f28721bfd71f89
Luguna <Luguna86@gmail.com>
2014-12-11 22:33:54 -0600

    Added changed routing scheme to fit new page groupings.  Added corresponding controllers.

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,15 +1,9 @@
-//var express = require('express');
-//var router = express.Router();
-//
-///* GET home page. */
-//router.get('/', function(req, res) {
-//    res.render('index', { title: 'Express' });
-//});
-//
-//module.exports = router;
+/* GET 'about us' page */
+module.exports.about = function(req, res) {
+    res.render('index', { title: 'About' });
+};
 
-// TODO:  See if there is a better way to wrap and expose this
-/* GET home page. */
-module.exports.index = function(req, res) {
-    res.render('index', { title: 'Express' });
+/* GET 'sign in' page */
+module.exports.signin = function(req, res) {
+    res.render('index', { title: 'Sign in' });
 };
