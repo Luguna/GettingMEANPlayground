88a859453ab54e0100bb79bedf911ac140294272
Luguna <Luguna86@gmail.com>
2014-12-14 21:15:53 -0600

    Set up mongoose connection to mongodb

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var fs = require('fs');
+require('./app_server/models/db');
 
 var app = express();
 
