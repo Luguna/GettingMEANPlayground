64d8105404336b52722eda292b3af3a9eab21f87
Luguna <Luguna86@gmail.com>
2014-12-27 00:05:26 -0600

    Added user geolocation awareness through browser support.  Homepage will show locations near user's geolocation.

--- public/angular/loc8rApp.js
+++ public/angular/loc8rApp.js
@@ -1,19 +1,59 @@
 angular.module('loc8rApp', []);
 
 var loc8rData = function($http) {
-    return $http.get('/api/locations?lng=-0.79&lat=51.3&maxDistance=30');
-}
+    var locationByCoords = function(lat, lng) {
+        return $http.get('/api/locations?lng=' + lng + '&lat=' + lat + '&maxDistance=30000');
+    };
+    return {
+        locationByCoords : locationByCoords
+    };
+    //return $http.get('/api/locations?lng=-0.79&lat=51.3&maxDistance=30');
+};
 
-var locationListCtrl = function ($scope, loc8rData) {
-    $scope.message = "Searching for nearby places...";
-    loc8rData
-        .success(function(data) {
-            $scope.message = data.length > 0 ? "" : "No locations found";
-            $scope.data = { locations: data };
-        })
-        .error(function(e) {
-            $scope.message = "Oh noes something has gone wrong!";
+var geolocation = function () {
+    var getPosition = function (cbSuccess, cbError, cbNoGeo) {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(cbSuccess, cbError);
+        }
+        else {
+            cbNoGeo();
+        }
+    };
+    return {
+        getPosition : getPosition
+    };
+};
+
+var locationListCtrl = function ($scope, loc8rData, geolocation) {
+    $scope.message = "Checking your location";
+
+    $scope.getData = function (position) {
+        var lat = position.coords.latitude;
+        var lng = position.coords.longitude;
+        $scope.message = "Searching for nearby places";
+        loc8rData.locationByCoords(lat, lng)
+            .success(function(data) {
+                $scope.message = data.length > 0 ? "" : "No locations found near Lat: " + lat + " Lng: " + lng;
+                $scope.data = { locations: data };
+            })
+            .error(function (e) {
+                $scope.message = "Oh noes something has gone wrong!";
+            });
+    };
+
+    $scope.showError = function (error) {
+        $scope.$apply(function() {
+            $scope.message = error.message;
         });
+    };
+
+    $scope.noGeo = function () {
+        $scope.$apply(function() {
+            $scope.message = "Geolocation not supported by this browser.";
+        });
+    };
+
+    geolocation.getPosition($scope.getData,$scope.showError,$scope.noGeo);
 };
 
 var _isNumeric = function (n) {
@@ -52,5 +92,6 @@ angular
     .controller('locationListCtrl', locationListCtrl)
     .filter('formatDistance', formatDistance)
     .directive('ratingStars', ratingStars)
-    .service('loc8rData', loc8rData);
+    .service('loc8rData', loc8rData)
+    .service('geolocation', geolocation);
 
